c8c6604b0796530885a8881b29d3006c26ba94c1
BrunoRFigueredo <37082376+BrunoRFigueredo@users.noreply.github.com>
2021-09-10 20:58:41 -0300

    Setup inicial da aplicação

---
+++ src/main/java/br/senac/devweb/ecommerceapiproduct/GeekECommerceApiProductApplication.java
@@ -0,0 +1,13 @@
+package br.senac.devweb.ecommerceapiproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekECommerceApiProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeekECommerceApiProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senac/devweb/ecommerceapiproduct/GeekECommerceApiProductApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.devweb.ecommerceapiproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekECommerceApiProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
